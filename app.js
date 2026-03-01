9487bd6d12da1a615c798aba0fa7c47042a1a339
Olivier <goussin.olivier@protonmail.com>
2026-03-01 21:15:01 +0100

    set-up terminé => faire une UI mvp

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
@@ -12,7 +11,7 @@ app.use(express.static("public"));
 
 app.use(
   session({
-    secret: process.env.SESSION_KEY,
+    secret: "key",
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -31,3 +30,91 @@ function requireAuth(req, res, next) {
 
   next();
 }
+
+// affiche la page d'inscription
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { prenom, chambre, mdp } = req.body;
+
+  // assertions de sécurité
+  if (!/^[0-9]{1,4}/.test(chambre))
+    return res.send("Numéro de chambre invalide");
+  if (mdp.length < 8) return res.send("Mot de passe trop court");
+
+  const hash = await bcrypt.hash(mdp, 12);
+
+  // ajouter le bonhomme à la db
+  try {
+    db.prepare(
+      `
+      INSERT INTO members (chambre, prenom, password)
+      VALUES (?, ?)
+      `,
+    ).run(chambre, prenom, password);
+
+    // inscrit, maintenant faut se connecter
+    res.redirect("/login");
+  } catch {
+    res.send("Problème lors de l'inscription.");
+  }
+});
+
+// page de connection
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const { prenom, mdp } = req.body;
+
+  const user = db.prepare(`SELECT * FROM members WHERE prenom = ?`).get(prenom);
+  if (!user) return res.send("Utilisateur introuvable :(");
+
+  const valid = bcrypt.compare(mdp, user.password);
+
+  if (!valid) return res.send("Mot de passe incorrect :(");
+
+  req.session.userId = user.id;
+  res.redirect("/dashboard");
+});
+
+// dashboard
+app.get("/dashboard", requireAuth, (req, res) => {
+  // pour le moment rangé par du plus récent au plus vieux, on changera ça plus tard
+  const objects = db
+    .prepare(
+      `
+    SELECT * FROM objects ORDER BY created_at DESC
+    `,
+    )
+    .all();
+
+  res.render("dashboard", { objects });
+});
+
+//ajouter un objet
+app.post("/add", requireAuth, (req, res) => {
+  const { titre, description } = req.body;
+
+  db.prepare(
+    `
+    INSERT INTO objects (titre, description, auteur)
+    VALUES (?,?,?)
+    `,
+  ).run(titre, description, req.session.userId);
+
+  res.redirect("/dashboard");
+});
+
+//logout
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
+
+app.listen(3000, () => {
+  console.log("Serveur lancé : http://localhost:3000/");
+});
